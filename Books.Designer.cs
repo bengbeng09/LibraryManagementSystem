f3de003eb198f5d45893bbc8efc7d304a6053167
Chris Mer <mercado.chris000@gmail.com>
2022-11-12 21:20:34 +0800

    Books, ginaya nasa Books design sa whimsical, wla ginagawa mga button
    edit:
    tinanggal ko yung book_id at binago ko yung ibang text alignment sa DataGridView
    
    (di ko ma revert nung parang na push ko ata kanina) (amend ata to )

--- Books.Designer.cs
+++ Books.Designer.cs
@@ -28,49 +28,264 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.btnDetails = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.lblSortBy = new System.Windows.Forms.Label();
+            this.lblSearch = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.title = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.edition = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.category = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.author = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.publisher = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.publication = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.copies = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // btnDetails
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(464, 438);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(149, 25);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "MANAGE BOOKS";
+            this.btnDetails.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(124)))), ((int)(((byte)(37)))), ((int)(((byte)(23)))));
+            this.btnDetails.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDetails.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnDetails.ForeColor = System.Drawing.Color.White;
+            this.btnDetails.Location = new System.Drawing.Point(56, 1082);
+            this.btnDetails.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnDetails.Name = "btnDetails";
+            this.btnDetails.Size = new System.Drawing.Size(123, 52);
+            this.btnDetails.TabIndex = 1;
+            this.btnDetails.Text = "DETAILS";
+            this.btnDetails.UseVisualStyleBackColor = false;
+            this.btnDetails.Click += new System.EventHandler(this.btnDetails_Click);
             // 
-            // button1
+            // btnDelete
             // 
-            this.button1.Location = new System.Drawing.Point(136, 147);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(112, 34);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(124)))), ((int)(((byte)(37)))), ((int)(((byte)(23)))));
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDelete.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnDelete.ForeColor = System.Drawing.Color.White;
+            this.btnDelete.Location = new System.Drawing.Point(604, 1082);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(123, 52);
+            this.btnDelete.TabIndex = 0;
+            this.btnDelete.Text = "DELETE";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(124)))), ((int)(((byte)(37)))), ((int)(((byte)(23)))));
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnAdd.ForeColor = System.Drawing.Color.White;
+            this.btnAdd.Location = new System.Drawing.Point(891, 1082);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(123, 52);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "ADD";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.AllowDrop = true;
+            this.btnUpdate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(124)))), ((int)(((byte)(37)))), ((int)(((byte)(23)))));
+            this.btnUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUpdate.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnUpdate.ForeColor = System.Drawing.Color.White;
+            this.btnUpdate.Location = new System.Drawing.Point(749, 1082);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(123, 52);
+            this.btnUpdate.TabIndex = 2;
+            this.btnUpdate.Text = "UPDATE";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            // 
+            // lblSortBy
+            // 
+            this.lblSortBy.AutoSize = true;
+            this.lblSortBy.Location = new System.Drawing.Point(604, 81);
+            this.lblSortBy.Name = "lblSortBy";
+            this.lblSortBy.Size = new System.Drawing.Size(79, 25);
+            this.lblSortBy.TabIndex = 5;
+            this.lblSortBy.Text = "SORT BY";
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Location = new System.Drawing.Point(804, 81);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(78, 25);
+            this.lblSearch.TabIndex = 6;
+            this.lblSearch.Text = "SEARCH";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtSearch.Location = new System.Drawing.Point(804, 120);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(210, 31);
+            this.txtSearch.TabIndex = 7;
+            // 
+            // listBox1
+            // 
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listBox1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 28;
+            this.listBox1.Items.AddRange(new object[] {
+            "TITLE",
+            "EDITION",
+            "CATEGORY",
+            "AUTHOR",
+            "PUBLISHER",
+            "PUBLICATION",
+            "COPIES",
+            "STATUS"});
+            this.listBox1.Location = new System.Drawing.Point(604, 120);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(175, 30);
+            this.listBox1.TabIndex = 9;
+            // 
+            // dgv
+            // 
+            this.dgv.BackgroundColor = System.Drawing.Color.White;
+            this.dgv.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.title,
+            this.edition,
+            this.category,
+            this.author,
+            this.status,
+            this.publisher,
+            this.publication,
+            this.copies});
+            this.dgv.Location = new System.Drawing.Point(56, 174);
+            this.dgv.Name = "dgv";
+            this.dgv.RowHeadersWidth = 62;
+            this.dgv.RowTemplate.Height = 33;
+            this.dgv.Size = new System.Drawing.Size(958, 887);
+            this.dgv.TabIndex = 10;
+            // 
+            // title
+            // 
+            this.title.DataPropertyName = "title";
+            this.title.HeaderText = "Title";
+            this.title.MinimumWidth = 8;
+            this.title.Name = "title";
+            this.title.Width = 200;
+            // 
+            // edition
+            // 
+            this.edition.DataPropertyName = "edition";
+            this.edition.HeaderText = "Edition";
+            this.edition.MinimumWidth = 8;
+            this.edition.Name = "edition";
+            this.edition.Width = 200;
+            // 
+            // category
+            // 
+            this.category.DataPropertyName = "category";
+            this.category.HeaderText = "Category";
+            this.category.MinimumWidth = 8;
+            this.category.Name = "category";
+            this.category.Width = 150;
+            // 
+            // author
+            // 
+            this.author.DataPropertyName = "author";
+            this.author.HeaderText = "Author";
+            this.author.MinimumWidth = 8;
+            this.author.Name = "author";
+            this.author.Width = 150;
+            // 
+            // status
+            // 
+            this.status.DataPropertyName = "status";
+            this.status.HeaderText = "Status";
+            this.status.MinimumWidth = 8;
+            this.status.Name = "status";
+            this.status.Width = 150;
+            // 
+            // publisher
+            // 
+            this.publisher.DataPropertyName = "publisher";
+            this.publisher.HeaderText = "Publisher";
+            this.publisher.MinimumWidth = 8;
+            this.publisher.Name = "publisher";
+            this.publisher.Width = 150;
+            // 
+            // publication
+            // 
+            this.publication.DataPropertyName = "publication";
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.publication.DefaultCellStyle = dataGridViewCellStyle1;
+            this.publication.HeaderText = "Publication";
+            this.publication.MinimumWidth = 8;
+            this.publication.Name = "publication";
+            this.publication.Width = 150;
+            // 
+            // copies
+            // 
+            this.copies.DataPropertyName = "copies";
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
+            this.copies.DefaultCellStyle = dataGridViewCellStyle2;
+            this.copies.HeaderText = "Copies";
+            this.copies.MinimumWidth = 8;
+            this.copies.Name = "copies";
+            this.copies.Width = 150;
             // 
             // Books
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1070, 1223);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnDetails);
+            this.Controls.Add(this.lblSearch);
+            this.Controls.Add(this.lblSortBy);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Books";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Books";
+            this.Load += new System.EventHandler(this.Books_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private Label label1;
-        private Button button1;
+        private Button btnDetails;
+        private Button btnDelete;
+        private Button btnAdd;
+        private Button btnUpdate;
+        private Label lblSortBy;
+        private Label lblSearch;
+        private TextBox txtSearch;
+        private ListBox listBox1;
+        private DataGridView dgv;
+        private DataGridViewTextBoxColumn title;
+        private DataGridViewTextBoxColumn edition;
+        private DataGridViewTextBoxColumn category;
+        private DataGridViewTextBoxColumn author;
+        private DataGridViewTextBoxColumn status;
+        private DataGridViewTextBoxColumn publisher;
+        private DataGridViewTextBoxColumn publication;
+        private DataGridViewTextBoxColumn copies;
     }
 }
--- Books.cs
+++ Books.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,31 @@ namespace LibraryManagementSystem
         public Books()
         {
             InitializeComponent();
+        }
+
+        private void cmbxSortBy_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDetails_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Books_Load(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection("Server=DELL16GB\\SQLEXPRESS;Initial Catalog=db_library;Integrated Security=True"))
+            {
+                con.Open();
+                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Books", con);
+                DataTable dtbl = new DataTable();
+                da.Fill(dtbl);
+                dgv.AutoGenerateColumns = false;
+                dgv.DataSource = dtbl;
+                
+                con.Close();
+            }
         }
     }
 }
